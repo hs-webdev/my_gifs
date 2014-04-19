6d5ffb8dc081461358a568027d44b35be85e3831
Arjun Venkataswamy <you@example.com>
2014-04-19 00:49:05 +0000

    dries up actions with route placeholders and conditional logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 MyGifs::Application.routes.draw do
+  get '/gifs/:id' => 'application#show'
   get '/gifs/1' => 'application#first'
   get '/gifs/2' => 'application#second'
   get '/gifs/3' => 'application#third'
