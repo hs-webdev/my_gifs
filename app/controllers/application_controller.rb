aad7dfce55779797e39cd97644dfc8926b40fa99
Arjun Venkataswamy <you@example.com>
2014-04-22 20:59:32 +0000

    pulls gif information from database instead of controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,16 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def show 
-    if params['id'] == '1'
-      @url = "http://f.cl.ly/items/0R0D3B18221l0H242F3L/tumblr_mbbxc9DUdK1rnvwt1.gif"
-      @caption = "Overwhelmed"
-    elsif params['id'] == '2'
-      @url = "http://media.tumblr.com/tumblr_mabgi9JAVC1rtufvk.gif"
-      @caption = "Exhausted"
-    elsif params['id'] == '3'
-      @url = "http://www.reactiongifs.com/wp-content/uploads/2012/06/swag_dance.gif"
-      @caption = "Swag"
-    end
+    @gif = Gif.find_by_id(params['id'])
     render 'show'
   end
 end
