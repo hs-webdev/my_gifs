59818ba48ca25bfff94c8ad65fa7dc412989768b
Arjun Venkataswamy <you@example.com>
2014-05-06 20:25:58 +0000

    organizes routes with CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 MyGifs::Application.routes.draw do
-  get '/gifs/:id' => 'application#show'
+  
+  # CREATE
   get '/new_gif' => 'application#new'
   get '/create_gif' => 'application#create'
+  
+  # READ
+  get '/gifs/:id' => 'application#show'
+  get '/gifs' => 'application#index'
+  
+  # UPDATE
   get '/gifs/:id/edit' => 'application#edit'
   get '/update_gif/:id' => 'application#update'
+  
+  # DESTROY
   get '/gifs/:id/destroy' => 'application#destroy'
-  get '/gifs' => 'application#index'
+ 
 end
 
